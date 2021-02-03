4040e5a12c1993a9db5c4931c9f046b26954a85c
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-02-03 20:37:13 +0300

    Update pycharm.sh

--- pycharm.sh
+++ pycharm.sh
@@ -1,1 +1,10 @@
-echo "hi"
+apt update
+apt install python3 wget
+mkdir /usr/share/pycharm
+wget https://download-cf.jetbrains.com/python/pycharm-community-2020.3.3.tar.gz -P /usr/share/pycharm
+tar -xvf /usr/share/pycharm/pycharm-community-2020.3.3.tar.gz
+rm /usr/share/pycharm/pycharm-community-2020.3.3.tar.gz
+mv /usr/share/pycharm/pycharm-community-2020.3.3/* /usr/share/pycharm
+rm -rf /usr/share/pycharm/pycharm-community-2020.3.3
+if [ ! -f /usr/bin/pycharm ]; then echo "bash /usr/share/pycharm/bin/pycharm.sh" > /usr/bin/pycharm; fi
+chmod +x /usr/bin/pycharm
