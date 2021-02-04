3fc595b554205e6f53702c0c7116ee3667871071
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-02-04 05:56:31 +0300

    Update idea.sh

--- idea.sh
+++ idea.sh
@@ -1,10 +1,10 @@
 apt update
 apt install openjdk-14-jdk wget
-mkdir /usr/share/idea
+if [ -d /usr/bin/idea ]; then rm -f /usr/bin/idea && mkdir /usr/bin/idea; fi
 wget https://download-cf.jetbrains.com/idea/ideaIC-2020.3.2.tar.gz -P /usr/share/idea
-tar -xvf /usr/share/idea/ideaIC-2020.3.2.tar.gz
+tar -xvf /usr/share/idea/ideaIC-2020.3.2.tar.gz -C /usr/bin/idea
 rm /usr/share/idea/ideaIC-2020.3.2.tar.gz
-mv /usr/share/idea/Idea-IC-*/* /usr/share/idea
-rm -rf /usr/share/idea/Idea-IC-*
+mv /usr/share/idea/idea-IC-*/* /usr/share/idea
+rm -rf /usr/share/idea/idea-IC-*
 if [ ! -f /usr/bin/idea ]; then echo "bash /usr/share/idea/bin/idea.sh" > /usr/bin/idea; fi
 chmod +x /usr/bin/idea
