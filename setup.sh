89b3bfcbbe13ed6276366063d314083420aa903e
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-02-06 02:26:11 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ case `dpkg --print-architecture` in
 if [ -f ~/linux-${archurl}.tar.xz ]; then rm -rf ~/linux-${archurl}.tar.xz; fi
 if [ -d ~/kali-fs ]; then rm -rf ~/kali-fs; fi
 if [ -d ~/kali-binds ]; then rm -rf ~/kali-binds; fi
-wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/linux-${archurl}.tar.xz
+wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/linux-${archurl}.tar.xz -P ~
 mkdir ~/kali-fs && mkdir ~/kali-binds 
 tar -xvf ~/linux-${archurl}.tar.xz -C ~/kali-fs || : 
 rm ~/linux-${archurl}.tar.xz
@@ -31,4 +31,4 @@ wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc
 wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/version && mv version ./kali-fs/proc/
 termux-setup-storage 
 if [ -d .termux ]; then rm -rf .termux && mkdir .termux && wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/termux.properties && mv termux.properties .termux;else mkdir .termux && wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/termux.properties && mv termux.properties .termux; fi
-echo -e "clear && termux-wake-lock && echo -e \"\\u001b[32m############################\\n#      IDE On Android      #\\n#       Version: 1.0       #\\n#     by: Hussein Faleh    #\\n############################\\u001b[39m\" && bash ~/start.sh && exit" > ~/.bashrc && source ~/.bashrc
+wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/bashrc -P ~ && mv ~/bashrc ~/.bashrc && source ~/.bashrc
