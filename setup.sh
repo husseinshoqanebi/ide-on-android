85913e4dfebb966a008dd01bc5a5b238cbab4ac5
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-02-06 08:33:28 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,14 +17,14 @@ case `dpkg --print-architecture` in
 	esac
 if [ -f ~/linux-${archurl}.tar.xz ]; then rm -rf ~/linux-${archurl}.tar.xz; fi
 if [ -d ~/kali-fs ]; then rm -rf ~/kali-fs; fi
-if [ -d ~/kali-binds ]; then rm -rf ~/kali-binds; fi
 wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/linux-${archurl}.tar.xz -P ~
 mkdir ~/kali-fs && mkdir ~/kali-binds 
 proot --link2symlink tar -xvf ~/linux-${archurl}.tar.xz -C ~/kali-fs || : 
 rm ~/linux-${archurl}.tar.xz
 if [ -f ~/start.sh ]; then rm -rf ~/start.sh; fi
-wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/start.sh
+wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/start.sh -P ~ && chmod +x ~/start.sh
 echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" > ~/kali-fs/etc/apt/sources.list
+echo "deb-src http://http.kali.org/kali kali-rolling main non-free contrib" >> ~/kali-fs/etc/apt/sources.list
 wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/loadavg && mv loadavg ~/kali-fs/proc/
 wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/stat && mv stat ~/kali-fs/proc/
 wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/uptime && mv uptime ~/kali-fs/proc/
