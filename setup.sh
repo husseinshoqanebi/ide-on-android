857ecca3b923c01501c4570b2d7e72d7c1341fd1
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-03-20 04:07:43 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,8 @@ if [ -f ~/linux-${archurl}.tar.xz ]; then rm -rf ~/linux-${archurl}.tar.xz; fi
 if [ -d ~/kali-fs ]; then rm -rf ~/kali-fs; fi
 wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/linux-${archurl}.tar.xz -P ~
 mkdir ~/kali-fs && mkdir ~/kali-binds 
-proot --link2symlink tar -xvf ~/linux-${archurl}.tar.xz -C ~/kali-fs || : 
+echo -e "\e[32mdecompressing the system file\e[37m ..."
+proot --link2symlink tar -xf ~/linux-${archurl}.tar.xz -C ~/kali-fs || : 
 rm ~/linux-${archurl}.tar.xz
 if [ -f ~/start.sh ]; then rm -rf ~/start.sh; fi
 wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/start.sh -P ~ && chmod +x ~/start.sh
