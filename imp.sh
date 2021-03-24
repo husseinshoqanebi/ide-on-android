079335b6179269a46972d3e327626e2ed50139a8
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-03-24 08:36:53 +0300

    Update imp.sh

--- imp.sh
+++ imp.sh
@@ -1,3 +1,3 @@
 apt update
-apt install net-tools iputils-ping dialog apt-utils iproute2 wget curl nano alsa-utils -y 
+apt install net-tools iputils-ping dialog apt-utils iproute2 wget curl nano alsa-utils sudo -y 
 apt clean
