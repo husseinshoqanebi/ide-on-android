00f637e54d855654dd3d1ee5e620abb6039df33f
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-03-31 11:28:35 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,28 +18,28 @@ case `dpkg --print-architecture` in
 	esac
 if [ -f ~/linux-${archurl}.tar.xz ]; then rm -rf ~/linux-${archurl}.tar.xz; fi
 if [ -d ~/kali-fs ]; then rm -rf ~/kali-fs; fi
-wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/linux-${archurl}.tar.xz -P ~
+wget https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/linux-${archurl}.tar.xz -P ~
 mkdir ~/kali-fs && mkdir ~/kali-binds 
 echo -e "\e[32mdecompressing the system file...\e[37m"
 proot --link2symlink tar -xf ~/linux-${archurl}.tar.xz -C ~/kali-fs || :
 rm ~/linux-${archurl}.tar.xz
 if [ -f ~/start.sh ]; then rm -rf ~/start.sh; fi
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/start.sh | cat > ~/start.sh && chmod +x ~/start.sh
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/start.sh | cat > ~/start.sh && chmod +x ~/start.sh
 echo "deb [trasted=yes] http://http.kali.org/kali kali-rolling main non-free contrib" > ~/kali-fs/etc/apt/sources.list
 echo "deb-src [trasted=yes] http://http.kali.org/kali kali-rolling main non-free contrib" >> ~/kali-fs/etc/apt/sources.list
 echo "deb [trasted=yes] http://httpredir.debian.org/debian jessie main" > ~/kali-fs/etc/apt/sources.list.d/debian.list
 echo "deb [trasted=yes] http://httpredir.debian.org/debian stretch main" >> ~/kali-fs/etc/apt/sources.list.d/debian.list
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/loadavg | cat > ~/kali-fs/proc/loadavg
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/stat | cat > ~/kali-fs/proc/stat
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/uptime | cat > ~/kali-fs/proc/uptime
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/vmstat | cat > ~/kali-fs/proc/vmstat
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/proc/version | cat > ~/kali-fs/proc/version
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/shadow | cat > ~/kali-fs/etc/shadow
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/bash.bashrc | cat > ~/kali-fs/etc/bash.bashrc
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/archive-key.asc | cat > ~/kali-fs/root/archive-key.asc
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/loadavg | cat > ~/kali-fs/proc/loadavg
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/stat | cat > ~/kali-fs/proc/stat
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/uptime | cat > ~/kali-fs/proc/uptime
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/vmstat | cat > ~/kali-fs/proc/vmstat
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/version | cat > ~/kali-fs/proc/version
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/shadow | cat > ~/kali-fs/etc/shadow
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/bash.bashrc | cat > ~/kali-fs/etc/bash.bashrc
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/archive-key.asc | cat > ~/kali-fs/root/archive-key.asc
 termux-setup-storage
-if [ -d .termux ]; then rm -rf .termux && mkdir .termux && cur https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/termux.properties && mv termux.properties .termux;else mkdir .termux && wget https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/termux.properties | cat > .termux/termux.propereties; fi
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-on-android/main/bashrc | cat > /data/data/com.termux/files/usr/etc/bash.bashrc
+if [ -d .termux ]; then rm -rf .termux && mkdir .termux && cur https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/termux.properties && mv termux.properties .termux;else mkdir .termux && wget https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/termux.properties | cat > .termux/termux.propereties; fi
+curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/bashrc | cat > /data/data/com.termux/files/usr/etc/bash.bashrc
 ## edit sign key of apt
 cd $(dirname $0)
 unset LD_PRELOAD
