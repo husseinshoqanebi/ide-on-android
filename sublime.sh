9c8e9b15c8ac58af97a8842b5382037f57791a3b
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-03-31 08:38:28 +0300

    Delete sublime.sh

--- sublime.sh
+++
@@ -1,4 +0,0 @@
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-apt install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
-apt update && apt install sublime-text
