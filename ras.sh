2b2b9a4a779f48b62c62ad092a9a1e12461813de
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-04-08 09:53:40 +0300

    Update ras.sh

--- ras.sh
+++ ras.sh
@@ -1,4 +1,5 @@
-echo "deb http://archive.raspbian.org/raspbian wheezy main contrib non-free" > /etc/apt/sources.list.d/raspbian.list
+#echo "deb http://archive.raspbian.org/raspbian wheezy main contrib non-free" > /etc/apt/sources.list.d/raspbian.list
 #echo "deb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free" >> /etc/apt/sources.list.d/raspbian.list
-wget https://archive.raspbian.org/raspbian.public.key -O - | sudo apt-key add -
-apt-get update
+#wget https://archive.raspbian.org/raspbian.public.key -O - | sudo apt-key add -
+#apt-get update
+curl https://raw.githubusercontent.com/futurejones/code-oss-aarch64/master/debian_stretch_armv7/vs-stretch-build.sh | bash
