1656df4d08215b23b6239e13cca9fec4b6612f6d
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-04-08 09:39:45 +0300

    Update ras.sh

--- ras.sh
+++ ras.sh
@@ -1,4 +1,4 @@
 echo "deb http://archive.raspbian.org/raspbian wheezy main contrib non-free" > /etc/apt/sources.list.d/raspbian.list
-echo "deb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free" >> /etc/apt/sources.list.d/raspbian.list
+#echo "deb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free" >> /etc/apt/sources.list.d/raspbian.list
 wget https://archive.raspbian.org/raspbian.public.key -O - | sudo apt-key add -
 apt-get update
