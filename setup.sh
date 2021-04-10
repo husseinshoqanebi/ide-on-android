ea56650f66c5e6878a3775420310751aedc9f508
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-04-10 12:20:11 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
+repo="ide-for-android"
 apt update && apt install wget openssl-tool proot -y
 case `dpkg --print-architecture` in
 	aarch64)
@@ -16,35 +17,56 @@ case `dpkg --print-architecture` in
 	*)
 		echo "unknown architecture"; exit 1 ;;
 	esac
-if [ -f ~/linux-${archurl}.tar.xz ]; then rm -rf ~/linux-${archurl}.tar.xz; fi
-if [ -d ~/kali-fs ]; then rm -rf ~/kali-fs; fi
-wget https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/linux-${archurl}.tar.xz -P ~
+rm -rf ~/linux-${archurl}.tar.xz
+rm -rf ~/kali-fs
+wget https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/linux-${archurl}.tar.xz -P ~
 mkdir ~/kali-fs && mkdir ~/kali-binds 
-echo -e "\e[32mdecompressing the system file...\e[37m"
+echo -e "\e[32mdecompressing the system file\e[37m"
 proot --link2symlink tar -xf ~/linux-${archurl}.tar.xz -C ~/kali-fs || :
 rm ~/linux-${archurl}.tar.xz
-if [ -f ~/start.sh ]; then rm -rf ~/start.sh; fi
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/start.sh | cat > ~/start.sh && chmod +x ~/start.sh
+rm -rf ~/start.sh
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/start.sh | cat > ~/start.sh && chmod +x ~/start.sh
+
+#add kali and debian repos
 echo "deb [trasted=yes] http://http.kali.org/kali kali-rolling main non-free contrib" > ~/kali-fs/etc/apt/sources.list
 echo "deb-src [trasted=yes] http://http.kali.org/kali kali-rolling main non-free contrib" >> ~/kali-fs/etc/apt/sources.list
 echo "deb [trasted=yes] http://httpredir.debian.org/debian jessie main" > ~/kali-fs/etc/apt/sources.list.d/debian.list
 echo "deb [trasted=yes] http://httpredir.debian.org/debian stretch main" >> ~/kali-fs/etc/apt/sources.list.d/debian.list
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/loadavg | cat > ~/kali-fs/proc/loadavg
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/stat | cat > ~/kali-fs/proc/stat
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/uptime | cat > ~/kali-fs/proc/uptime
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/vmstat | cat > ~/kali-fs/proc/vmstat
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/proc/version | cat > ~/kali-fs/proc/version
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/shadow | cat > ~/kali-fs/etc/shadow
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/bash.bashrc | cat > ~/kali-fs/etc/bash.bashrc
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/archive-key.asc | cat > ~/kali-fs/root/archive-key.asc
+
+#fix processes problem
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/proc/loadavg | cat > ~/kali-fs/proc/loadavg
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/proc/stat | cat > ~/kali-fs/proc/stat
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/proc/uptime | cat > ~/kali-fs/proc/uptime
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/proc/vmstat | cat > ~/kali-fs/proc/vmstat
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/proc/version | cat > ~/kali-fs/proc/version
+
+#change root default password
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/shadow | cat > ~/kali-fs/etc/shadow
+
+#color the bash and add importent stuffes
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/bash.bashrc | cat > ~/kali-fs/etc/bash.bashrc
+
+#give permission to termux to access storage
 termux-setup-storage
-if [ -d .termux ]; then rm -rf .termux && mkdir .termux && cur https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/termux.properties && mv termux.properties .termux;else mkdir .termux && wget https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/termux.properties | cat > .termux/termux.propereties; fi
-curl https://raw.githubusercontent.com/husseinshoqanebi/ide-for-android/main/bashrc | cat > /data/data/com.termux/files/usr/etc/bash.bashrc
-## edit sign key of apt
+
+#edit termux keyborad
+rm -rf .termux
+mkdir .termux
+cur https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/termux.properties && mv termux.properties .termux
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/bashrc | cat > /data/data/com.termux/files/usr/etc/bash.bashrc
+
+#edit sign key of apt
 cd $(dirname $0)
 unset LD_PRELOAD
+curl https://raw.githubusercontent.com/husseinshoqanebi/${repo}/main/archive-key.asc | cat > ~/kali-fs/root/archive-key.asc
 proot --kernel-release=5.10.0 --link2symlink --kill-on-exit --root-id -r kali-fs -b /dev -b /sys -b /proc -b kali-fs/root:/dev/shm -w /root /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM TMPDIR=/tmp LANG=C.UTF-8 /bin/apt-key add archive-key.asc >/dev/null 2>&1
-rm /data/data/com.termux/files/usr/etc/motd
 rm /data/data/com.termux/files/home/kali-fs/root/archive-key.asc
-bash ~/start.sh
-exit
+
+#remove termux init message
+rm /data/data/com.termux/files/usr/etc/motd
+
+#start
+exec bash
+
+#first run exit after linux exit
+pkill -9 bash
