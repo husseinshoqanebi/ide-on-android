f5502241106b9b1e7ea58bc5a6649f93bc3c9e48
husseinshoqanebi <husseinshoqanebi@gmail.com>
2021-05-17 17:18:41 +0300

    Create install-ssh.sh

---
+++ install-ssh.sh
@@ -0,0 +1,5 @@
+rm -rf /etc/dropbear
+mkdir /etc/dropbear
+dropbearkey -t dss -s 1024 -f /etc/dropbear/dropbear_dss_host_key
+dropbearkey -t rsa -s 2048 -f /etc/dropbear/dropbear_rsa_host_key
+dropbearkey -t ecdsa -s 521 -f /etc/dropbear/dropbear_ecdsa_host_key
